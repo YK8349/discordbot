a42c90523e499f8511f3f6a2d9d3f87ce096b6a9
YK8349 <157105465+YK8349@users.noreply.github.com>
2025-07-01 18:30:27 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ client = discord.Client(intents=intents)
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
 
-model = genai.GenerativeModel('gemini-Flash') # 使用するモデルを指定
+model = genai.GenerativeModel('gemini-2.5-flash-lite') # 使用するモデルを指定
 
 GUILD_ID = 1127013631763169301  # テスト用サーバーIDに置き換えてください（任意）
 
